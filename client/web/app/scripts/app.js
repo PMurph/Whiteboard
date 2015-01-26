c4fcfe23ac1540788ae7033cbcd76065b351951e
Reyad Attiyat <reyad.attiyat@gmail.com>
2015-01-26 00:47:18 -0600

    Web Client: FIX: Move initializers to main
    This commit fixes a behaviour where app will be undefined unless the initlizers are in a different files.
    I think this is becuase the initalizers depend on the "app" variable from requireJS.

--- client/web/app/scripts/app.js
+++ client/web/app/scripts/app.js
@@ -1,60 +1,7 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'marionette',
-    'controllers/MainController',
-    'routers/MainRouter',
-    'controllers/UserSession',
-    'views/master'
-], function (
-    $,
-    _,
-    Backbone,
-    Marionette,
-    Controller,
-    Router,
-    MasterView) {
+    'marionette'
+], function (Marionette) {
     'use strict';
-
-    var App = new Marionette.Application();
-
-    App.addRegions({
-        'body': 'body'
-    });
-
-    App.addInitializer(function() {
-        var mainView = new MasterView();
-        var controller = new Controller();
-
-        this.userSessionController = new UserSessionController();
-
-        App.body.show(mainView);
-        controller.mainContent = mainView.mainContent;
-
-        new Router({
-            controller: controller
-        });
-    });
-
-    App.addInitializer(function() {
-        $.ajaxSetup({
-            cache: false,
-            crossDomain: true,
-            dataType: 'jsonp',
-            headers: {
-                'Authorization': 'session id here'
-            },
-            statusCode: {
-                // Forbidden
-                403: function() {
-                    Backbone.history.navigate('login', {trigger: true});
-                }
-            }
-        });
-
-        Backbone.history.start({ pushState: false, root: '/' });
-    });
-
-    return App;
+    
+    return new Marionette.Application();
 });
