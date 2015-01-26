90caea4746e8f43de06ee93da3fdcf8eda0b6ba0
Spartibartfast <fussy.tiger@gmail.com>
2015-01-26 04:15:48 +0000

    Adding factory stuff to RoomFactory, now creates a basic room and whiteboard

--- server/src/room_objects/RoomFactory.js
+++ server/src/room_objects/RoomFactory.js
@@ -1,4 +1,6 @@
 'use strict';
+var Room = require('room');
+var Whiteboard = require('whiteboard')
 
 var RoomFactory = function(creatingUser) {
     "use strict"
@@ -9,14 +11,19 @@ RoomFactory.prototype = {
     initialize: function() {
         creatingUser: String
     },
+    // Room(roomId, creatingUser, whiteboard)
     _createNewRoom: function(creatingUser) {
-        var anoncreatingUser = new this.RoomModel({
-            this.creatingUser: creatingUser,
-            anonymous: true
-        });
+        this.creatingUser = creatingUser;
+        this.newRoomID = _getNextRoomID();
+        
+        var newWhiteboard = Whiteboard();
+        var newRoom = Room(newRoomID, creatingUser, newWhiteboard);
 
-        return anoncreatingUser;
+        return newRoom;
     },
+    _getNextRoomID: function() {
+        return 0;
+    }
 });
 
 module.exports = RoomFactory;
