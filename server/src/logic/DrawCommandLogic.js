46297f8f1b3b884811de18f97edb268273f9e3f9
Patrick <murph.pm@gmail.com>
2015-01-28 17:21:02 -0600

    Added the skeleton for the DrawCommandLogic class

---
+++ server/src/logic/DrawCommandLogic.js
@@ -0,0 +1,11 @@
+var DrawCommandLogic = function(room) {
+    this._room = room;
+};
+
+DrawCommandLogic.prototype = {
+    handleDrawCommand: function(drawCommandWrapper) {
+        this._room.handleDrawCommand(drawCommandWrapper);
+    },
+};
+
+module.exports.DrawCommandLogic = DrawCommandLogic;
---
+++ server/tests/logic/DrawCommandLogicSpec.js
@@ -0,0 +1,26 @@
+var logic = require("../../src/logic/DrawCommandLogic.js");
+
+describe("DrawCommandLogic", function() {
+    var roomMock;
+    var drawCommandWrapperMock;
+    var testDrawCommandLogic;
+
+    beforeEach(function() {
+        roomMock = {
+            handleDrawCommand: function(drawCommandWrapper) {},
+        };
+
+        drawCommandWrapperMock = {
+
+        };
+
+        testDrawCommandLogic = new logic.DrawCommandLogic(roomMock);
+    });
+
+    it("should call the room's handleDrawCommand function with the same drawCommandWrapper if the wrapper is valid", function() {
+        spyOn(roomMock, "handleDrawCommand");
+
+        testDrawCommandLogic.handleDrawCommand(drawCommandWrapperMock);
+        expect(roomMock.handleDrawCommand).toHaveBeenCalledWith(drawCommandWrapperMock);
+    });
+});
