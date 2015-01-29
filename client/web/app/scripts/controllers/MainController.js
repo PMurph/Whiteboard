89cd6a0ad85c38c36bfcb154d649f7ce56dfa6d9
Reyad Attiyat <reyad.attiyat@gmail.com>
2015-01-29 00:41:12 -0600

    Web Client: Move the ajax setup inside MainController
    I tried to clean up how the app starts up. Please note Backbone.history.start() cannont be moved esaily.

--- client/web/app/scripts/controllers/MainController.js
+++ client/web/app/scripts/controllers/MainController.js
@@ -1,14 +1,36 @@
 define([
+    'jquery',
+    'backbone',
     'marionette',
     'views/dashboard',
     'views/login/layout'
 ], function(
+    $,
+    Backbone,
     Marionette,
     Dashboard,
     LoginView) {
     'use strict';
 
     return Marionette.Controller.extend({
+        _setupAjax: function() {
+            $.ajaxSetup({
+                cache: false,
+                crossDomain: true,
+                dataType: 'jsonp',
+                contentType: 'application/json',
+                statusCode: {
+                    // Forbidden
+                    403: function() {
+                        Backbone.history.navigate('login', {trigger: true});
+                        //self.login();
+                    }
+                }
+            });
+        },
+        initialize: function() {
+            this._setupAjax();
+        },
         login: function() {
             this.mainContent.show(new LoginView());
         },
--- client/web/app/scripts/main.js
+++ client/web/app/scripts/main.js
@@ -24,40 +24,25 @@ require([
 
     App.on('start', function() {
         this.mainController = new MainController();
+        this.router = new Router({
+            controller: this.mainController
+        });
         this.mainView = new MasterView({
             controller: this.mainController
         });
 
         this.userSessionController = new UserSessionController();
-        this.userSessionController.authAnonymous();
+        //this.userSessionController.authAnonymous();
+        this.userSessionController.authUser("test", "password1");
 
         this.body.show(this.mainView);
         this.mainController.mainContent = this.mainView.mainContent;
 
-        new Router({
-            controller: this.mainController
-        });
     });
 
     App.on('start', function() {
-        $.ajaxSetup({
-            cache: false,
-            crossDomain: true,
-            dataType: 'jsonp',
-            headers: {
-                'Authorization': 'session id here'
-            },
-            statusCode: {
-                // Forbidden
-                403: function() {
-                    Backbone.history.navigate('login', {trigger: true});
-                }
-            }
-        });
-
         Backbone.history.start({ pushState: false, root: '/' });
     });
-
 
     App.start();
 });
