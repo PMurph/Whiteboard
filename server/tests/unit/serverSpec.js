4ba6cf9db63f96f4e77f5064851159d1b8dc1da7
Reyad Attiyat <reyad.attiyat@gmail.com>
2015-02-02 21:36:13 -0600

    Server: Test: Add server unit tests
    This commit tests that the server object can start.
    It also test setting options.

--- server/tests/unit/serverSpec.js
+++ server/tests/unit/serverSpec.js
@@ -1,14 +1,58 @@
 "use strict";
 
 describe("Server", function() {
-    var Server = require('../../src/server');
+    var Server = require("../../src/server");
+    var mockMongoose = require("mongoose-mock");
+    var mockExpress = jasmine.createSpyObj("express", ["use","listen"]);
     var server;
-    beforeAll(function() {
-       server = new Server(); 
+    beforeEach(function() {
+        var dbOptions = {
+            mongoose: mockMongoose
+        };
+        server = new Server(mockExpress, dbOptions); 
     });
     it("should start", function() {
+        server.start();
+        expect(mockExpress.listen).toHaveBeenCalled();
     });
-    it("should stop", function() {
-        expect(true).toBe(true);
+    it("should set port number on start", function() {
+        var testPort = 3456;
+        server.start(testPort);
+
+        expect(server.getPort()).toBe(testPort);
+    });
+    it("should set port number and hostname on start", function() {
+        var testPort = 3456;
+        var testHostname = "testHostname";
+
+        server.start(testPort, testHostname);
+        expect(server.getHostname()).toBe(testHostname);
+    });
+    it("should set port number and hostname on start", function() {
+        var testPort = 3456;
+        var testHostname = "testHostname";
+
+        server.start(testPort, testHostname);
+        expect(server.getHostname()).toBe(testHostname);
+    });
+    it("should set database hostname", function() {
+        var testHostname = "testHostname";
+        var testDBOptions = {
+            mongoose: mockMongoose,
+            hostname: testHostname
+        };
+        server = new Server(mockExpress, testDBOptions);
+
+        expect(server.getDBHostname()).toBe(testHostname);
+    });
+    it("should set database name", function() {
+        var testDBName = "testDBname";
+        var testDBOptions = {
+            mongoose: mockMongoose,
+            name: testDBName
+        };
+        server = new Server(mockExpress, testDBOptions);
+
+        expect(server.getDBName()).toBe(testDBName);
     });
 });
