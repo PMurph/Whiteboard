9a66dede2be145883c15716d0d4bedd26d3b4f8d
Reyad Attiyat <reyad.attiyat@gmail.com>
2015-02-02 21:32:58 -0600

    Server: Add support for mocking the server and changing database options.
    This allows for testing of the server object unit.

--- server/src/server.js
+++ server/src/server.js
@@ -1,51 +1,70 @@
 'use strict';
 
-var express = require('express'),
-    bodyParser = require('body-parser'),
+var bodyParser = require('body-parser'),
+    Express = require('express'),
     mongoose = require('mongoose'),
     UserManager = require('./session/UserManager');
 
-var Server = function () {
-    this.app = express();
+var Server = function (exp, dbOptions) {
+    this.app = exp;
     this._db = mongoose;
     
-    this.port = process.env.PORT || 3000;
-    this.hostname = '0.0.0.0';
-    this.dbHostname = '127.0.0.1';
+    this._port = process.env.PORT || 3000;
+    this._hostname = '0.0.0.0';
+    this._dbHostname = '127.0.0.1';
+    this._dbName = 'whiteboard';
+
+    if(dbOptions) {
+        this._db = dbOptions.mongoose || this._db;
+        this._dbHostname = dbOptions.hostname || this._dbHostname;
+        this._dbName = dbOptions.name || this._dbName;
+    }
 
     this._userManager = new UserManager(this._db);
-    
+
     this._setupMiddleware();
     this._setupRoutes();
 
 };
 
 Server.prototype = {
-    _connectDB: function(hostname) {
-        mongoose.connect('mongodb://' + hostname + '/whiteboard');
+    _connectDB: function(hostname, dbName) {
+        this._db.connect('mongodb://' + hostname + '/' + dbName);
     },
     _disconnectDB: function() {
-        mongoose.disconnect();
+        this._db.disconnect();
     },
     _setupMiddleware: function() {
         this.app.use(bodyParser.json());
     },
     _setupRoutes: function() {
-        this.app.use('/', express.static(__dirname + '/../../client/web/app'));
+        this.app.use('/', Express.static(__dirname + '/../../client/web/app'));
         this.app.use('/api/user', this._userManager.getRouteF());
     },
     start: function(port, hostname, listenCB) {
-        this.port = port || this.port;
-        this.hostname = hostname || this.hostname;
-        this._connectDB(this.dbHostname);
-        this.httpServ = this.app.listen(this.port, this.hostname, null, listenCB);
+        this._port = port || this._port;
+        this._hostname = hostname || this._hostname;
+    //    this._connectDB(this.dbHostname, this.dbName);
+        this._httpServ = this.app.listen(this._port, this._hostname, null, listenCB);
     },
     stop: function(closeCB) {
-        this._disconnectDB();
-        if(this.httpServ){
+        //this._disconnectDB();
+        if(this._httpServ){
             console.log('Stopping server...');
-            this.httpServ.close(closeCB);
+            this._httpServ.close(closeCB);
         }
+    },
+    getPort: function() {
+        return this._port;
+    },
+    getHostname: function() {
+        return this._hostname;
+    },
+    getDBHostname: function() {
+        return this._dbHostname;
+    },
+    getDBName: function() {
+        return this._dbName;
     }
 };
 
