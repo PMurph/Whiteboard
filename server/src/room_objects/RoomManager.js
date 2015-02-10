2e8b2b6eed025246ad8cfab10d535adef14bd994
Patrick <murph.pm@gmail.com>
2015-02-10 00:16:51 -0600

    Added part of the user authentication logic to the logic for an incoming join room request

--- server/src/room_objects/RoomManager.js
+++ server/src/room_objects/RoomManager.js
@@ -1,37 +1,63 @@
 'use strict';
 var RoomFactory = require('./RoomFactory');
 
-var RoomManager = function(socketManager) {
-    this.roomID = 0;
-    this.roomFactory = new RoomFactory();
-    this.rooms = {};
+var RoomManager = function(socketManager, userManager) {
+    this._roomFactory = new RoomFactory();
+    this._rooms = {};
+    this._userManager = userManager;
+    this._pendingJoinRoomRequests = {};
     
     this.initSocketCallbacks(socketManager);
 };
 
 RoomManager.prototype = ({
+    authenticateUserCallback: function(error, user) {
+        if(error || !user || !this._pendingJoinRoomRequests[user.authToken]) {
+            
+        } else {
+            var pendingJoinRequest = this._pendingJoinRoomRequests[user.authToken];
+            this._joinRoom(pendingJoinRequest.roomId, pendingJoinRequest.socket);
+            delete this._pendingJoinRoomRequests[user.authToken];
+        }
+    },
+
+    _handleUnathenticatedRequest: function() {
+        var i = 0;
+        i++;
+    },
+
+    _joinRoom: function(roomId, socket) {
+        if(this._rooms[roomId]) {
+            socket.join(roomId);
+        }
+    },
+
     initSocketCallbacks: function(socketManager) {
         var self = this;
         socketManager.on("connection", function(socket) {
             socket.on("joinRequest", function(msgData) {
-                self.joinRoom(msgData.roomdId, socket);
+                self.handleJoinRequest(msgData.roomId, msgData.authToken, socket);
             });
         });
     },
 
+    handleJoinRequest: function(roomId, authToken, socket) {
+        var self = this;
+        this._pendingJoinRoomRequests[authToken] = {roomId: roomId, socket: socket};
+        this._userManager.findByAuthToken(authToken, function(error, user) {
+            self.authenticateUserCallback(error, user);
+        });
+    },
+
     createNewRoom: function(creatingUser) {
-        var room = this.roomFactory.createNewRoom(creatingUser);
+        var room = this._roomFactory.createNewRoom(creatingUser);
         var roomId = room.getId();
-        this.rooms[roomId] = room;
+        this._rooms[roomId] = room;
         return roomId;
     },
 
     getRoom: function(roomId) {
-        return this.rooms[roomId];
-    },
-
-    joinRoom: function(roomId, socket) {
-        socket.join(roomId);
+        return this._rooms[roomId];
     },
 });
 
--- server/tests/unit/room_objects/RoomManagerSpec.js
+++ server/tests/unit/room_objects/RoomManagerSpec.js
@@ -7,11 +7,13 @@ describe("RoomManager", function() {
     var testRoomManager;
     var testRoomId;
     var socketManagerMock;
+    var userManagerMock;
 
     beforeEach(function() {
-        socketManagerMock = jasmine.createSpy("socketManager", ["on"]);
+        socketManagerMock = jasmine.createSpyObj("socketManager", ["on"]);
+        userManagerMock = jasmine.createSpyObj("UserManager", ["findByAuthToken"]);
 
-        testRoomManager = new RoomManager(socketManagerMock);
+        testRoomManager = new RoomManager(socketManagerMock, userManagerMock);
         testRoomManager.createNewRoom(TEST_USER_NAME);
         testRoomId = testRoomManager.createNewRoom(TEST_USER_NAME);
     });
@@ -28,16 +30,29 @@ describe("RoomManager", function() {
         var socketMock;
 
         beforeEach(function() {
-            socketMock = jasmine.createSpy('socketManager', ["join"]);
+            socketMock = jasmine.createSpyObj('socketManager', ["join"]);
         });
 
-        it("should allow a user to join a room that exists", function() {
-            testRoomManager.joinRoom(testRoomId, socketMock);
-            expect(socketMock.join).haveBeenCalledWith(testRoomId);
-        });
+        describe("valid user", function() {
+            var TEST_AUTH_TOKEN = "ValidToken";
+            var TEST_USER = {authToken: TEST_AUTH_TOKEN};
 
-        it("should not allow a user to join a room that does not exist", function() {
+            it("should authenticate user using the authentication tocken when a room request occurs", function() {
+                testRoomManager.handleJoinRequest(testRoomId, TEST_AUTH_TOKEN, socketMock);
+                expect(userManagerMock.findByAuthToken).toHaveBeenCalledWith(TEST_AUTH_TOKEN, jasmine.any(Function));
+            });
 
+            it("should allow a user to join a room that exists", function() {
+                testRoomManager.handleJoinRequest(testRoomId, TEST_AUTH_TOKEN, socketMock);
+                testRoomManager.authenticateUserCallback(null, TEST_USER);
+                expect(socketMock.join).toHaveBeenCalledWith(testRoomId);
+            });
+
+            it("should not allow a user to join a room that does not exist", function() {
+                testRoomManager.handleJoinRequest(42, TEST_AUTH_TOKEN, socketMock);
+                testRoomManager.authenticateUserCallback(null, TEST_USER);
+                expect(socketMock.join).not.toHaveBeenCalled();
+            });
         });
     });
 });
