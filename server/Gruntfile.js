292f1a91bf04c01889f4376c7104a4c0e7dc61ea
PMurph <murph.pm@gmail.com>
2015-02-24 18:34:14 -0600

    Added grunt task and jasmine runner for integration tests

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -59,14 +59,14 @@ module.exports = function (grunt) {
                     failOnError: false
                 }
             },
-            jasmineRunnerIntegration: {
-                command: 'node tests/jasmineRunner integration',
+            jasmineRunnerInteg: {
+                command: 'node tests/jasmineRunner integ',
                 options: {
                     failOnError: false
                 }
             },
             jasmineRunnerAll: {
-                command: 'node tests/jasmineRunner unit integration',
+                command: 'node tests/jasmineRunner unit integ',
                 options: {
                     failOnError: false
                 }
@@ -131,8 +131,12 @@ module.exports = function (grunt) {
         startMongo(['shell:jasmineRunnerAll']);
     });
 
-    grunt.registerTask('test',[
-                'shell:jasmineRunnerUnit'
+    grunt.registerTask('test-unit',[
+        'shell:jasmineRunnerUnit'
+    ]);
+    
+    grunt.registerTask('test-integ',[
+        'shell:jasmineRunnerInteg'
     ]);
 
     grunt.registerTask('default', [
--- server/tests/jasmineRunner.js
+++ server/tests/jasmineRunner.js
@@ -11,6 +11,8 @@ var terminalReporter = new reporters.TerminalReporter({
 var j = new Jasmine();
 if (process.argv[2] === "unit") {
     j.loadConfigFile("tests/unit.json");
+} else if (process.argv[2] === "integ") {
+    j.loadConfigFile("tests/integ.json");
 }
 j.addReporter(terminalReporter);
 
