60e098341f64ca1a9dae1a4c1d7d41a0d991a2ad
Chris Funk <umfunk59@hamster.cs.umanitoba.ca>
2015-03-13 13:53:57 -0500

    No more JS in there, all objectC, which isn't so bad I guess...

--- client/ios/WhiteboardiOS/WhiteboardiOS/DrawObject.h
+++ client/ios/WhiteboardiOS/WhiteboardiOS/DrawObject.h
@@ -9,58 +9,24 @@
 #import <Foundation/Foundation.h>
 
 @interface DrawObject : NSObject
-define(['backbone'], function (Backbone) {
-    'use strict';
+
+    typedef enum TOOLS : NSUInteger {
+        DRAW,
+        ERASE
+    } Tools;
     
-    var TOOLS = {
-        DRAW : { value: 0, name: "Draw"},
-        ERASE : { value: 1, name: "Erase" }
-    };
+    typedef enum  COLOURS : NSUInteger {
+        BLACK,
+        BLUE,
+        RED,
+        YELLOW,
+        GREEN,
+        PURPLE
+    } Colours;
     
-    var COLOURS = {
-        BLACK : { value: 0, name: "Black" },
-        BLUE : { value: 1, name: "Blue" },
-        RED : { value: 2, name: "Red" },
-        YELLOW : { value: 3, name: "Yellow" },
-        GREEN : { value: 4, name: "Green" },
-        PURPLE : { value: 5, name: "Purple" }
-    };
-    
-    var DrawObjects = Backbone.Model.extend({
-    url: '/api/drawobject',
-        
-    initialize: function() {
-        this._tool = TOOLS.DRAW;
-        this._colour = COLOURS.BLACK;
-    },
-        
-    setColour: function(newColour) {
-        for (var colourName in COLOURS) {
-            var colour = COLOURS[colourName];
-            
-            if (colour.name === newColour) {
-                this._colour = colour;
-                return true;
-            }
-        }
-        
-        return false;
-    },
-        
-    setTool: function(newTool) {
-        for (var toolName in TOOLS) {
-            var tool = TOOLS[toolName];
-            
-            if (tool.name === newTool) {
-                this._tool = tool;
-                return true;
-            }
-        }
-        
-        return false;
-    },
-        
-        -(NSString)getColour();
-        
--(NSString)getTool();
+-(id)init;
+-(void)setColour:(NSString*)newColour;
+-(void)setTool:(NSString*)newTool;
+-(NSString*)getColour;
+-(NSString*)getTool;
 @end
