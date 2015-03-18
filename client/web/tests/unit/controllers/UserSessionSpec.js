1bdea09955e587800197bee71f6917957075a329
Reyad Attiyat <reyad.attiyat@gmail.com>
2015-03-18 09:21:46 -0500

    Web Client: Fix tests and add some more tests

--- client/web/tests/unit/controllers/UserSessionSpec.js
+++ client/web/tests/unit/controllers/UserSessionSpec.js
@@ -3,10 +3,13 @@
 define(["squirejs/Squire"], function(Squire) {
     describe("User Session Controller", function() {
         var usc,
-            injector;
+            injector,
+            fireFail;
 
         var mockUser = function(){};
         beforeEach(function(done) {
+            fireFail = false;
+
             mockUser.prototype = jasmine.createSpyObj("user", ["setPassword", "setLogin", "getLogin", "getB64Password", "fetch", "save"]);
             mockUser.extend = jasmine.createSpy("userConstructor").and.callFake(function() {
                 return mockUser;   
@@ -14,12 +17,17 @@ define(["squirejs/Squire"], function(Squire) {
             var fakePromise = function() {
                 var promise = {
                     then: function(cb) {
-                        cb({
-                            authToken: "GoodToken"   
-                        });
-
+                        if (!fireFail) {
+                            cb({
+                                authToken: "GoodToken"   
+                            });
+                        }
                         return {
-                            fail: jasmine.createSpy("fail")
+                            fail: function(cb) {
+                                if (fireFail) {
+                                    cb("Failed");
+                                }
+                            }
                         };
                     }
                 };
@@ -41,6 +49,11 @@ define(["squirejs/Squire"], function(Squire) {
                 usc = new UserSessionController();
                 done();
             });
+
+        });
+        afterEach(function () {
+            usc.off("AuthFailed");
+            usc.off("Authenticated");
         });
         it("should authenticate user", function(done) {
             var testLogin = "testLogin",
@@ -48,7 +61,8 @@ define(["squirejs/Squire"], function(Squire) {
                 testSave = true;
  
             usc.on("Authenticated", function() {
-                expect(usc.getUser()).not.toBe(null);
+                expect(usc.getUser()).not.toBeNull();
+                expect(usc.isAuthenticated()).toBe(true);
                 expect(mockUser.prototype.fetch).toHaveBeenCalledWith({
                     data: jasmine.objectContaining({
                         saveSession: testSave
@@ -58,25 +72,76 @@ define(["squirejs/Squire"], function(Squire) {
             });
             usc.authUser(testLogin, testPassword, testSave);
         });
+        it("should FAIL to authenticate user", function(done) {
+            var testLogin = "testLogin",
+                testPassword = "testPassword",
+                testSave = true;
+ 
+            usc.on("AuthFailed", function() {
+                expect(usc.getUser()).toBeNull();
+                expect(usc.isAuthenticated()).toBe(false);
+                done();
+            });
+            fireFail = true;
+            usc.authUser(testLogin, testPassword, testSave);
+        });
         it("should authenticate anonymous", function(done) {
             usc.on("Authenticated", function() {
-                expect(usc.getUser()).not.toBe(null);
+                expect(usc.getUser()).not.toBeNull();
                 expect(mockUser.prototype.save).toHaveBeenCalledWith({});
+                expect(usc.isAuthenticated()).toBe(true);
                 done();
             });
             usc.authAnonymous(); 
         });
-        it("should register user", function() {
+        it("should FAIL to authenticate anonymous", function(done) {
+            usc.on("AuthFailed", function() {
+                expect(usc.getUser()).toBeNull();
+                expect(mockUser.prototype.save).toHaveBeenCalledWith({});
+                expect(usc.isAuthenticated()).toBe(false);
+                done();
+            });
+            fireFail = true;
+            usc.authAnonymous(); 
+        });
+        it("should register user", function(done) {
             var testLogin = "newLogin",
                 testPassword = "newPassword",
                 B64Password = window.btoa(testPassword),
                 testSave = true;
+
+            usc.on("Authenticated", function() {
+                expect(usc.getUser()).not.toBeNull();
+                expect(usc.isAuthenticated()).toBe(true);
+                expect(mockUser.prototype.save).toHaveBeenCalledWith(jasmine.objectContaining({
+                    login: testLogin,
+                    b64password: B64Password,
+                    saveSession: testSave
+                }), {wait: true});
+                done();
+            });
+
             usc.registerUser(testLogin, testPassword, testSave);
-            expect(mockUser.prototype.save).toHaveBeenCalledWith(jasmine.objectContaining({
-                login: testLogin,
-                b64password: B64Password,
-                saveSession: testSave
-            }), {wait: true});
+        });
+        it("should FAIL to register user", function(done) {
+            var testLogin = "newLogin",
+                testPassword = "newPassword",
+                B64Password = window.btoa(testPassword),
+                testSave = false;
+
+            usc.on("AuthFailed", function() {
+                expect(usc.getUser()).toBeNull();
+                expect(usc.isAuthenticated()).toBe(false);
+                expect(mockUser.prototype.save).toHaveBeenCalledWith(jasmine.objectContaining({
+                    login: testLogin,
+                    b64password: B64Password,
+                    saveSession: testSave
+                }), {wait: true});
+                done();
+            });
+            
+            fireFail = true;
+            usc.registerUser(testLogin, testPassword, testSave);
         });
     });
 });
