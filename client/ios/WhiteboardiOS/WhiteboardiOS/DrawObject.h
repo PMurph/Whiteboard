47bf34ba3ebe2e2e657de0c0e1c97ed244687b7f
Spartibartfast <fussy.tiger@gmail.com>
2015-03-22 18:22:06 -0500

    Fixing some stuff

--- client/ios/WhiteboardiOS/WhiteboardiOS/DrawObject.h
+++ client/ios/WhiteboardiOS/WhiteboardiOS/DrawObject.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface DrawObject : NSObject
-    typedef enum TOOLS : NSString {
+    typedef enum TOOLS : NSUInteger {
         DRAW,
         ERASE
     } Tools;
