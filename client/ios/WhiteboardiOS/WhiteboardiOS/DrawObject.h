45d1bf0458bed794a9be43e6da9229193f6df103
Spartibartfast <fussy.tiger@gmail.com>
2015-03-22 18:42:28 -0500

    Changing dictionarys to have property value, so we can read from them if need be

--- client/ios/WhiteboardiOS/WhiteboardiOS/DrawObject.h
+++ client/ios/WhiteboardiOS/WhiteboardiOS/DrawObject.h
@@ -23,11 +23,11 @@
         PURPLE
     } Colours;
 
-    NSDictionary* colourMap;
-    NSDictionary* toolMap;
-
 @property (readonly, nonatomic) Colours myColour;
 @property (readonly, nonatomic) Tools myTool;
+
+@property (readonly, nonatomic) NSDictionary* colourMap;
+@property (readonly, nonatomic) NSDictionary* toolMap;
 
 -(id)init;
 -(void)setColour:(NSString*)newColour;
