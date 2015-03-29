74151c5f074a502f64af5374c601953935ee1e95
Reyad Attiyat <reyad.attiyat@gmail.com>
2015-03-28 23:15:58 -0500

    iOS: Reskit Wrapper should not be strong

--- client/ios/WhiteboardiOS/WhiteboardiOS/AppDelegate.h
+++ client/ios/WhiteboardiOS/WhiteboardiOS/AppDelegate.h
@@ -1,16 +1,18 @@
 #import <UIKit/UIKit.h>
 
 #import "RestkitWrapper.h"
+#import "UserSession.h"
 #import "RoomManager.h"
 #import "UserSession.h"
+
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic, readwrite) RestkitWrapper *restkitWrapper;
 @property (strong, readonly) NSString *webAppURI;
-@property (strong, readonly) RoomManager *roomManager;
 @property (strong, readwrite) UserSession *userSession;
+@property (strong, readonly) RoomManager *roomManager;
 
 @end
 
