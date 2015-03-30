b696f866d6ac7136064150faaf2b4e4d59a08c2c
Patrick Murphy <murph.pm@gmail.com>
2015-03-30 12:44:26 -0500

    Updated js version of the test

--- client/ios/WhiteboardiOS/WhiteboardiOSTests/acceptance/js_scripts/user_session_tests.js
+++ client/ios/WhiteboardiOS/WhiteboardiOSTests/acceptance/js_scripts/user_session_tests.js
@@ -47,11 +47,16 @@ if(settingBtn.isValid()) {
     UIALogger.logFail(testName);
 }
 
+testName = "Logout of account";
+
+UIALogger.logStart(testName);
+UIALogger.logFail(testName);
+
 testName = "Login to valid account";
 
 UIALogger.logStart(testName);
-userNameField.setValue("SuperTestUser1");
-passwordField.setValue("password123");
+userNameField.setValue(testUserName);
+passwordField.setValue(testPassword);
 target.frontMostApp().mainWindow().buttons()["Login"].tap();
 target.pushTimeout(5);
 settingBtn = tabbar.buttons()["Settings"];
@@ -61,3 +66,37 @@ if(settingBtn.isValid()) {
 } else {
     UIALogger.logFail(testName);
 }
+
+testName = "Switch to settings";
+
+UIALogger.logStart(testName);
+tabbar.buttons()["Settings"].tap();
+target.pushTimeout(5);
+var saveBtn = target.frontMostApp().mainWindow().buttons()["Save"];
+target.popTimeout();
+if(saveBtn.isValid()) {
+    UIALogger.logPass(testName);
+} else {
+    UIALogger.logFail(testName);
+}
+
+testName = "User settings save successful";
+
+UIALogger.logStart(testName);
+var displayNameField = target.frontMostApp().mainWindow().textFields()[0];
+displayNameField.setValue("Sudsfdnvmncxv");
+saveBtn.tap();
+target.pushTimeout(5);
+var saveSuccessful = target.frontMostApp().mainWindow().staticTexts()["Save Successful"];
+target.popTimeout();
+if(saveSuccessful.isValid()) {
+    UIALogger.logPass(testName);
+} else {
+    UIALogger.logFail(testName);
+}
+
+testName = "User settings save fail";
+
+UIALogger.logStart(testName);
+
+UIAlogger.logFail(testName);
